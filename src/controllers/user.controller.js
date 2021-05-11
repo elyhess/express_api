adcf7d87934bcfbe3141ed2bde056f0afbfe5934
Ely Hess <ely.hess@me.com>
2021-05-11 11:18:32 -0600

    Add delete endpoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,18 @@
 const db = require("../models");
 const User = db.rest.models.user;
 
+exports.getUsers = async (req, res) => {
+    const users = await User.findAll();
+
+    if(!users) {
+        return res.status(400).send({
+            message: "No users found."
+        });
+    }
+
+    return res.send(users)
+}
+
 exports.getUser = async (req, res) => {
     const { id } = req.params;
 
@@ -84,5 +96,32 @@ exports.updateUser = async (req, res) => {
         return res.status(500).send({
             message: `Error ${err.message}`
         })
+    }
+};
+
+exports.deleteUser = async (req, res) => {
+    const { id } = req.params;
+
+    const user = await User.findOne({
+        where: {
+            id
+        },
+    });
+
+    if (!user) {
+        return res.status(400).send({
+            message: `User with id ${id} not found.`
+        });
+    }
+
+    try {
+        await user.destroy();
+        return res.send({
+            message: "User has been deleted"
+        });
+    } catch(err) {
+        return res.status(500).send({
+            message: `Error ${err.message}`
+        });
     }
 };
--- src/routes/user.js
+++ src/routes/user.js
@@ -3,7 +3,9 @@ const user = require('../controllers/user.controller');
 const router = Router();
 
 router.get("/:id", user.getUser); // GET /users/:id
+router.get("", user.getUsers); // GET /users
 router.post("", user.createUser) // POST /users
-router.patch("/:id", user.updateUser)
+router.patch("/:id", user.updateUser) // UPDATE /users/:id
+router.delete("/:id", user.deleteUser); // DELETE /users/:id
 
 export default router;
